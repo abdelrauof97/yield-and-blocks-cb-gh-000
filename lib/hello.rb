23ca163dd17f2f8ecfb3b754ba12521babc95f0c
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 16:20:02 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,9 +9,9 @@ end
 
 names = ["Tim", "Tom", "Jim"]
 
-hello_t(names) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+hello_t(names) #do |name|
+  #if name.start_with?("T")
+  #  puts "Hi, #{name}"
+ # end
+#end
 
